98eb3221ec2a5675c0b08962bdf79737fae511f2
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-06-28 15:40:09 +0200

    -Angular CLI Update -Angular Core Update -Rxjs Update -Ngrx Install

--- RecipeApp/src/app/app-routing.module.ts
+++ RecipeApp/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {RecipesComponent} from "./recipes/recipes.component";
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import {RecipesComponent} from "./Components/recipes/recipes.component";
+import {ShoppingListComponent} from "./Components/shopping-list/shopping-list.component";
+import {RecipeStartComponent} from "./Components/recipes/recipe-start/recipe-start.component";
+import {RecipeDetailComponent} from "./Components/recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./Components/recipes/recipe-edit/recipe-edit.component";
 import {RecipesResolver} from "./Resolvers/recipes.resolver";
 import {AuthComponent} from "./auth/auth.component";
 import {AuthGuard} from "./Guards/auth.guard";
--- RecipeApp/src/app/app.module.ts
+++ RecipeApp/src/app/app.module.ts
@@ -2,25 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { RecipesComponent } from './Components/recipes/recipes.component';
+import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './Components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './Components/shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { DropdownDirective } from './dropdown.directive';
+import { DropdownDirective } from '../shared/dropdown.directive';
 import {RecipeService} from "./Services/recipe.service";
 import {ShoppingListService} from "./Services/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './Components/recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './Components/recipes/recipe-edit/recipe-edit.component';
 import {DataStorageService} from "./Services/data-storage.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { AuthComponent } from './auth/auth.component';
 import {LoadingSpinnerComponent} from "../shared/loading-spinner/loading-spinner.component";
-import {AuthInterceptor} from "./Interceptors/auth.interceptor";
+import {AuthInterceptor} from "./auth/auth.interceptor";
+import { StoreModule } from '@ngrx/store';
 
 
 
@@ -45,7 +46,8 @@ import {AuthInterceptor} from "./Interceptors/auth.interceptor";
     FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [RecipeService,ShoppingListService,DataStorageService,
     {
